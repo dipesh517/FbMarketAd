22e4e7e2b79769b4e1bfc6987b47829f9c67a26c
dipesh labh <dipeshlav@gmail.com>
2021-12-15 22:50:37 +0545

    Updating bugs fixed

--- AdCampaign/urls.py
+++ AdCampaign/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('', views.CampaignList.as_view()),
-  path('<string:pk>/', views.CampaignDetail.as_view()),
+  path('<str:pk>/', views.CampaignDetail.as_view()),
   path('accountSecrets',views.AccountSecretsView.as_view())
 ]
 
