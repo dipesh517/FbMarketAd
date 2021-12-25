ac6b296979cafc2d55c79ae1882c3d2c4b5db50c
dipesh labh <dipeshlav@gmail.com>
2021-12-25 09:26:13 +0545

    url bug fixed

--- adset/urls.py
+++ adset/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.AdsetList.as_view()),
-  path('<int:pk>/', views.AdsetDetail.as_view()),
-  path('<int:pk>/adcreatives',views.AdCreativesListByAdset.as_view())
+  path('<int:pk>', views.AdsetDetail.as_view()),
+  path('<int:pk>/adcreatives',views.AdCreativesListByAdset.as_view()),
 ]
